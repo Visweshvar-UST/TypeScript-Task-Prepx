3b922dcc7083e39b03eb85b9e59011c835058830
Visweshvar-UST <287618@ust.com>
2024-10-17 12:38:27 +0530

    gen loc

---
+++ t1/employee-obj.ts
@@ -0,0 +1,12 @@
+const EmployeeObject = {
+    empId : "EMP12",
+    empName : "visweshvar",
+    email : "viswa@gmail.com"
+};
+console.log(EmployeeObject);
+
+
+interface empInterface {
+    id : number,
+    name : string
+};
